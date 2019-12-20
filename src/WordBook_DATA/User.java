573bf6ded70835b9ee556d484d9ccb7a181d375c
choyj0920 <choyj0920@naver.com>
2019-12-20 15:57:40 +0900

    깃깃

--- src/WordBook_DATA/User.java
+++ src/WordBook_DATA/User.java
@@ -2,5 +2,13 @@ package WordBook_DATA;
 
 public class User {
 	String userId,userPw;
-	
+	int testword;
+	User(String userId, String userPw, int testword){
+		this.userId=userId; this.userPw=userPw; this.testword=testword;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return userId+"\n"+userPw+"\n"+testword+"\n";
+	}
 }
--- src/WordBook_DATA/Worddata.java
+++ src/WordBook_DATA/Worddata.java
@@ -1,6 +1,8 @@
 package WordBook_DATA;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -9,18 +11,23 @@ import java.util.Scanner;
 
 public class Worddata {
 	
-	static ArrayList<String> userlist=new ArrayList<String>();
+	static LinkedHashMap<String,User> userlist=new LinkedHashMap<String, User>();
 	
-	String filename;
+	static String filename="words.txt";
 	
-	LinkedHashMap<String, Word> wordbook=new LinkedHashMap<String, Word>();
+	static LinkedHashMap<String, Word> wordBook=new LinkedHashMap<String, Word>();
+	
+	LinkedHashMap<String, Word> myWordBook=new LinkedHashMap<String, Word>();
+	
 	
 	static void LoadUserList() {
 		File load=new File("userdata/userlist.txt");
 		try {
 			Scanner scan= new Scanner(load);
 			while(scan.hasNext()) {
-				
+				User newuser= new User(scan.nextLine().trim(),
+						scan.nextLine().trim(),scan.nextInt());
+				scan.nextLine();
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
@@ -28,7 +35,33 @@ public class Worddata {
 		
 	}
 	static void saveUserList() {
-		File load=new File("userdata/userlist.txt");
+		File file=new File("userdata/userlist.txt");
+		//颇老 按眉 积己  
+	    try {
+	      FileWriter fw = new FileWriter(file);	      
+	      for(User u: userlist.values()) {
+	    	  fw.write(u.toString());
+	      }
+	      fw.close();
+	      
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+	}
+	boolean login(String iD,String pW) {
+		if(userlist.containsKey(iD) &&
+				userlist.get(iD).userPw.equals(pW)) {
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	Worddata(String username){
+		String filepath= "userdata/"+username+".txt";
 		
 	}
+	
+	
+	
 }
