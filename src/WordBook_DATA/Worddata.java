74173c3a3dcc35e2cfe4fc98d316fa0172ad399e
choyj0920 <choyj0920@naver.com>
2019-12-20 14:25:37 +0900

    데이터

--- src/WordBook_DATA/Worddata.java
+++ src/WordBook_DATA/Worddata.java
@@ -1,5 +1,17 @@
 package WordBook_DATA;
 
-public class Worddata {
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Worddata {
+	
+	static ArrayList<String> userlist=new ArrayList<String>();
+	
+	String filename;
+	
+	LinkedHashMap<String, Word> wordbook=new LinkedHashMap<String, Word>();
+	
+	static void Loaduserlist();
 }
